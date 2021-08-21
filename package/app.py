5b438fe7e641642384380432668c8cd903f48c37
k_yamanaka <vaivailx+git@gmail.com>
2021-08-21 14:25:51 +0900

    ログ出力について追加

--- package/app.py
+++ package/app.py
@@ -2,9 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from logging import getLogger, config
+
+
+import json
+
+with open("log_config.json", "r") as f:
+    log_conf = json.load(f)
+    config.dictConfig(log_conf)
+
+logger = getLogger("app")
+
 
 @app.route("/")
 def index():
+    logger.info("called index.")
     return "<span style='color:red'>I am app 1</span>"
 
 
